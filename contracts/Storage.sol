84d969807ecfd27dea364f184ffd94e1e50851c7
Otto G <ottodevs@gmail.com>
2019-06-12 18:47:59 +0200

    Contract fixes and type change for value

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.24;
 
-import "@aragon/os/contracts/apps/AragonApp.sol";
+import "@aragon/core/contracts/apps/AragonApp.sol";
 
 
 contract Storage is AragonApp {
@@ -9,11 +9,11 @@ contract Storage is AragonApp {
     event Registered(bytes32 indexed key);
 
     struct DataEntry {
-        bytes32 value
+        string value;
     }
 
     /// State: data registry
-    mapping(bytes32 => DataEntry) internal registeredData;
+    mapping(bytes32 => string) internal registeredData;
 
     /// ACL
     bytes32 constant public REGISTER_DATA_ROLE = keccak256("REGISTER_DATA_ROLE");
@@ -24,18 +24,18 @@ contract Storage is AragonApp {
     }
 
     /**
-     * @notice Set `_key` data tohome app to `app`
-     * @param app App that will be set as home app
+     * @notice Set `_key` data to `_value`
+     * @param _key Data item that will be stored in the registry
+     * @param _value Data content to be stored
      */
-    function registerData(bytes32 _key, bytes32 _value) external auth(REGISTER_DATA_ROLE) {
-        // TODO: check that _app is an app proxy
-        // TODO: check that _app is installed in this org
-        bytes32 storage dataEntry = registeredData[_key];
-        dataEntry.value = _value
-        emit Registered(_app);
+    function registerData(bytes32 _key, string _value) external auth(REGISTER_DATA_ROLE) {
+        // TODO: check that _key is an app proxy
+        // TODO: check that _key is installed in this org
+        registeredData[_key] = _value;
+        emit Registered(_key);
     }
 
-    function getRegisteredData(bytes32 _key) external view returns(bytes32) {
+    function getRegisteredData(bytes32 _key) external view returns(string) {
         return registeredData[_key];
     }
 }
