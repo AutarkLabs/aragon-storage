29470398100f026c4f27e8224623687df790ace8
Otto G <ottodevs@gmail.com>
2019-06-12 17:58:16 +0200

    Add key-value registry to contract

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -6,26 +6,36 @@ import "@aragon/os/contracts/apps/AragonApp.sol";
 contract Storage is AragonApp {
 
     /// Events
-    event Registered(address homeApp);
+    event Registered(bytes32 indexed key);
 
-    /// State
-    address public homeApp;
+    struct DataEntry {
+        bytes32 value
+    }
+
+    /// State: data registry
+    mapping(bytes32 => DataEntry) internal registeredData;
 
     /// ACL
     bytes32 constant public REGISTER_DATA_ROLE = keccak256("REGISTER_DATA_ROLE");
 
+    /// Custom aragon constructor
     function initialize() public onlyInit {
         initialized();
     }
 
     /**
-     * @notice Set home app to `app`
+     * @notice Set `_key` data tohome app to `app`
      * @param app App that will be set as home app
      */
-    function setApp(address _app) external auth(REGISTER_DATA_ROLE) {
+    function registerData(bytes32 _key, bytes32 _value) external auth(REGISTER_DATA_ROLE) {
         // TODO: check that _app is an app proxy
         // TODO: check that _app is installed in this org
-        homeApp = app;
-        emit Register(_app);
+        bytes32 storage dataEntry = registeredData[_key];
+        dataEntry.value = _value
+        emit Registered(_app);
+    }
+
+    function getRegisteredData(bytes32 _key) external view returns(bytes32) {
+        return registeredData[_key];
     }
 }
