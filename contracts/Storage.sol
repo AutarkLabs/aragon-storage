0510409040d4b7df9643d8d3212b20dc37605d9b
Otto G <ottodevs@gmail.com>
2019-08-22 13:50:58 +0200

    Initial approach to multiple settings/single tx

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -9,7 +9,7 @@ contract Storage is AragonApp {
     event Registered(bytes32 indexed key);
 
     /// State: data registry
-    mapping(bytes32 => string) internal registeredData;
+    mapping(bytes32 => bytes32) internal registeredData;
 
     /// ACL
     bytes32 constant public REGISTER_DATA_ROLE = keccak256("REGISTER_DATA_ROLE");
@@ -20,19 +20,25 @@ contract Storage is AragonApp {
     }
 
     /**
-     * @notice Set `_key` data to `_value`
-     * @param _key Data item that will be stored in the registry
-     * @param _value Data content to be stored
+     * @notice Set `_keys` data to `_values`
+     * @param _keys Data item that will be stored in the registry
+     * @param _values Data content to be stored
      */
+    function changeMultipleSettings(bytes32[] _keys, bytes32[] _values) external auth(REGISTER_DATA_ROLE) {
+        require(_keys.length == _values.length, "Keys and values length mismatch");
+        for (uint8 i = 0; i < _keys.length; i++) {
+            registerData(_keys[i], _values[i]);
+        }
+    }
 
-    function registerData(bytes32 _key, string _value) external auth(REGISTER_DATA_ROLE) {
+    function getRegisteredData(bytes32 _key) external view returns(bytes32) {
+        return registeredData[_key];
+    }
+
+    function registerData(bytes32 _key, bytes32 _value) internal {
         // TODO: check that _key is an app proxy
         // TODO: check that _key is installed in this org
         registeredData[_key] = _value;
         emit Registered(_key);
-    }
-
-    function getRegisteredData(bytes32 _key) external view returns(string) {
-        return registeredData[_key];
     }
 }
