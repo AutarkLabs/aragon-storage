7cce440243c9a7382b1b0838b3ef9978225ed394
Thomas O'Brien <tbobrien612@gmail.com>
2019-08-23 13:33:38 -0400

    WIP register storage provider contract

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -7,12 +7,22 @@ contract Storage is AragonApp {
 
     /// Events
     event Registered(bytes32 indexed key);
+    event RegisterStorageProvidver(string provider, string uri);
 
     /// State: data registry
     mapping(bytes32 => string) internal registeredData;
 
     /// ACL
     bytes32 constant public REGISTER_DATA_ROLE = keccak256("REGISTER_DATA_ROLE");
+    bytes32 constant public REGISTER_STORAGE_PROVIDER_ROLE = keccak256("REGISTER_STORAGE_PROVIDER_ROLE");
+
+    struct StorageProvider {
+        string provider;
+        string uri;
+        address setter;
+    }
+
+    StorageProvider storageProvider;
 
     /// Custom aragon constructor
     function initialize() public onlyInit {
@@ -34,5 +44,22 @@ contract Storage is AragonApp {
 
     function getRegisteredData(bytes32 _key) external view returns(string) {
         return registeredData[_key];
+    }
+
+    function registerStorageProvider(string provider, string uri) external auth(REGISTER_STORAGE_PROVIDER_ROLE) {
+        StorageProvider memory storageting = storageProvider;
+        storageting.provider = provider;
+        storageting.uri = uri;
+        storageting.setter = msg.sender;
+        emit RegisterStorageProvidver(provider, uri);
+    }
+
+    function getStorageProvider() external view returns(string, string, address) {
+        StorageProvider memory storageting = storageProvider;
+        return (
+            storageting.provider,
+            storageting.uri,
+            storageting.setter
+        );
     }
 }
