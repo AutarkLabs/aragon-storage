9ae6407f1e8d4278be8840992217fea24542290a
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2019-11-11 12:26:09 -0500

    remove register storage provider code

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -2,30 +2,20 @@ pragma solidity ^0.4.24;
 
 import "@aragon/os/contracts/apps/AragonApp.sol";
 
-
 contract Storage is AragonApp {
 
     /// Events
     event Registered(bytes32 indexed key);
     event PinHash(string cid);
-    event RegisterStorageProvider(string provider, string uri, uint64 port, address providerSetter);
 
     /// State: data registry
     mapping(bytes32 => string) internal registeredData;
 
     /// ACL
     bytes32 constant public REGISTER_DATA_ROLE = keccak256("REGISTER_DATA_ROLE");
-    bytes32 constant public REGISTER_STORAGE_PROVIDER_ROLE = keccak256("REGISTER_STORAGE_PROVIDER_ROLE");
-
-    string provider;
-    string uri;
-    uint64 port;
 
     /// Custom aragon constructor
     function initialize() public onlyInit {
-        provider = "aragon_association";
-        uri = "https://api.pinata.cloud";
-        port = 5001;
         initialized();
     }
 
@@ -43,20 +33,5 @@ contract Storage is AragonApp {
 
     function getRegisteredData(bytes32 _key) external view returns(string) {
         return registeredData[_key];
-    }
-
-    function registerStorageProvider(string newProvider, string newUri, uint64 newPort) external auth(REGISTER_STORAGE_PROVIDER_ROLE) {
-        provider = newProvider;
-        uri = newUri;
-        port = newPort;
-        emit RegisterStorageProvider(provider, uri, newPort, msg.sender);
-    }
-
-    function getStorageProvider() external view returns(string, string, uint64) {
-        return (
-            provider,
-            uri,
-            port
-        );
     }
 }
